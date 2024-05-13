ce00ffe8f649bb78609c4c7c841f15ca1556dc75
heoyoon <parechae@gmail.com>
2024-05-13 17:49:58 +0900

    feat : 백준 풀이 CPP로 한 것

--- CPlusPlusPractice/CPlusPlusPractice/main.cpp
+++ CPlusPlusPractice/CPlusPlusPractice/main.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    int A;
+    int B;
+    cin >> A>>B;
+    int bSize = to_string(B).length();
+    int numArr = 10;
+    int numbLine = 1;
+    for (int i = 0; i < bSize; i++)
+    {
+        if (i != 0)
+        {
+            numbLine = numArr;
+            numArr *= 10;
+        }
+        int targetLine = B % numArr;
+        cout << (targetLine / numbLine)*A << endl;
+    }
+    cout << A*B << endl;
+    return 0;
+}
 
 
 int sum(int a, int b) {
@@ -15,10 +41,16 @@ int sum(int a, int b) {
 //
 //    return 0;
 //}
-void main() 
-{
-    int a;
-    std::cout << INT32_MAX << std::endl;
-    scanf_s("%i", &a);
-    std::cout << a << std::endl;
-}
+
+//
+//void main()
+//{
+//    int a;
+//    std::cout << INT32_MAX << std::endl;
+//    scanf_s("%i", &a);
+//    //입력을 감지하는 함수호출,앞에 스트링으로 받는 값은 Int로 입력을 받는단뜻
+//    //&a의 &가 붙으면 해당 변수의 주소값을 가져오는것
+//    std::cout << a << std::endl;
+//}
+
+
