fa48547fbcf053b11646f8c6bb7f034a26095fc2
Heoyoon <parechae@gmail.com>
2024-08-19 18:55:57 +0900

    [level 2] Title: 예상 대진표, Time: 0.27 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -5,13 +5,18 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        while(a != b)
+        
+        while(n != 1)
         {
+            ++answer;
+            n /= 2;
             a = (a/2)+a%2;
             b = (b/2)+b%2;
-            answer++;
+            if(a==b )
+            {
+                break;
+            }
         }
-        
 
         return answer;
     }
