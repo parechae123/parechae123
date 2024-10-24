4d6898124e0bcc4e06426c11809547ebf706a34d
Heoyoon <parechae@gmail.com>
2024-10-24 18:46:56 +0900

    [level 2] Title: 프로세스, Time: 2.85 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 2.96 ms
+메모리: 31.7 MB, 시간: 2.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 24일 18:36:35
+2024년 10월 24일 18:46:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.cs
+++ 프로그래머스/2/42587. 프로세스/프로세스.cs
@@ -1,29 +1,32 @@
+
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
-    public int solution(int[] priorities, int location) {
+    public int solution(int[] priorities, int location)
+    {
         int answer = 0;
-        Queue<int> procQueue = new Queue<int>(priorities);
-        Array.Sort(priorities,(item1,item2) => item2.CompareTo(item1));
-        Queue<int> maxQueue = new Queue<int>(priorities);
-        while(maxQueue.Count > 0)
+        Queue<KeyValuePair<int, int>> que = new Queue<KeyValuePair<int, int>>();
+        for(int i = 0; i < priorities.Length; i++)
         {
-            location--;
-            if(maxQueue.Peek() == procQueue.Peek())
-            {
-                maxQueue.Dequeue();
-                procQueue.Dequeue();
-                answer++;
-                if(location == -1)break;
-            }
-            else
-            {
-                if(location == -1) location = procQueue.Count-1;
-                procQueue.Enqueue(procQueue.Dequeue());
-
-            }
+            que.Enqueue(new KeyValuePair<int, int>(i, priorities[i]));
         }
-        return answer;
+        while(true)
+        {
+            int nMax = que.Max(x => x.Value);
+            var kv = que.Dequeue();
+            if (kv.Value == nMax)
+            {
+                if (kv.Key == location) return answer + 1;
+                else
+                {
+                    answer++;
+                    continue;
+                }
+            }
+            que.Enqueue(kv);
+        }            
     }
 }
