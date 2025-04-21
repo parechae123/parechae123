346c596855eb6766cdb460d7028d41129737dde5
Heoyoon <parechae@gmail.com>
2025-04-21 13:21:21 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 2.03 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
@@ -1,28 +1,7 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 
 public class Solution {
     public string[] solution(string my_string) {
-        List<string> answer = new List<string>();
-        int index = 0;
-        answer.Add(String.Empty);
-        foreach(char result in my_string)
-        {
-
-            if(result != ' ')
-            {
-                answer[index] += result;
-            }
-            else
-            {
-                if(answer[index] ==String.Empty)continue;
-                answer.Add(String.Empty);
-                index++;
-                continue;
-            }
-        }
-        if(answer[index]== String.Empty)answer.RemoveAt(index);
-        return answer.ToArray();
+        return my_string.Split(' ',StringSplitOptions.RemoveEmptyEntries);
     }
 }
