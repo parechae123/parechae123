cd60654b74c93ef46da252f192cb74766e8c4033
Heoyoon <parechae@gmail.com>
2025-04-21 13:39:55 +0900

    [level 0] Title: ad 제거하기, Time: 1.65 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cs
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 public class Solution {
     public string[] solution(string[] strArr) {
-        List<string> tempList = strArr.ToList<string>();
-        tempList.RemoveAll(x=> x.Contains("ad"));
-        return tempList.ToArray();
+        return strArr.Where(x=>!x.Contains("ad")).ToArray();
     }
 }
