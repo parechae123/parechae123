bf17e4ec4164d12337c629d1f12cff8792619a5c
Heoyoon <parechae@gmail.com>
2025-04-28 18:19:47 +0900

    [level 0] Title: 짝수의 합, Time: 0.13 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -2,6 +2,6 @@ using System;
 using System.Linq;
 public class Solution {
     public int solution(int n) {
-        return Enumerable.Range(0,n+1).Where(x=>x%2 == 0).ToArray().Sum();
+        return n/2 * (n/2 + 1);
     }
 }
