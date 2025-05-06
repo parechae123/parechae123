023541204f68e670fa5eab6154a616c5315c7161
Heoyoon <parechae@gmail.com>
2025-05-06 17:13:41 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.51 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cs
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cs
@@ -3,6 +3,6 @@ using System.Linq;
 
 public class Solution {
     public string solution(string myString) {
-        return new string(myString.Select(x=>x >='a'? (char)(x-('a'-'A')) : (char)x).ToArray());
+        return myString.ToUpper();
     }
 }
