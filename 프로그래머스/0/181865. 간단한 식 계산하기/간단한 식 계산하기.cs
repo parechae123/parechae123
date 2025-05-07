5ce06494a4a1091a9877877589c499bb83f4d383
Heoyoon <parechae@gmail.com>
2025-05-07 18:37:42 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 1.08 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cs
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cs
@@ -3,28 +3,9 @@ using System.Linq;
 
 public class Solution {
     public int solution(string binomial) {
-        int answer = 0;
-        for(int i = '*'; i<= '-';i++)
-        {
-            if(i == 44) continue;
-            int operIdx = binomial.IndexOf((char)i);
-            if(operIdx == -1)continue;
-            
-            string[] numbers = binomial.Split((char)i);
-            switch((char)i)
-            {
-                case '-':
-                    answer = int.Parse(numbers[0]) - int.Parse(numbers[1]);
-                    break;
-                case '+':
-                    answer = int.Parse(numbers[0]) + int.Parse(numbers[1]);
-                    break;
-                case '*':
-                    answer = int.Parse(numbers[0]) * int.Parse(numbers[1]);
-                    break;
-            }
-            break;
-        }
-        return answer;
+        string[] numbers = binomial.Split(' ');
+        return numbers[1] == "*"? int.Parse(numbers[0])*int.Parse(numbers[2]) : 
+        numbers[1] == "-"? int.Parse(numbers[0])-int.Parse(numbers[2]) :
+        int.Parse(numbers[0])+int.Parse(numbers[2]);
     }
 }
