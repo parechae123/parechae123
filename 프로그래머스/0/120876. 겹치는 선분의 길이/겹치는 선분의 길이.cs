8d78a6332ab6fdd7c0a5572975c798f015ff85b1
Heoyoon <parechae@gmail.com>
2025-05-16 23:51:54 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 3.74 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cs
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cs
@@ -1,27 +1,26 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+
 public class Solution {
     public int solution(int[,] lines) {
-        
-        int[] dLines = Enumerable.Repeat(0,lines.GetLength(0)*lines.GetLength(1))
+        int[] flatLine = Enumerable.Repeat(0,lines.GetLength(0)*lines.GetLength(1))
             .Select((num,idx)=> lines[idx/lines.GetLength(1),idx%lines.GetLength(1)]).ToArray();
-        int min = dLines.Min();
-        int max = dLines.Max();
+        
+        int min = flatLine.Min();
+        int max = flatLine.Max();
+        
         min = min < 0? Math.Abs(min): 0;
-        max += min+1;
+        max += min;
         
-        dLines = Enumerable.Repeat(0,max).ToArray();
-        
-        for(int i = 0; i<lines.GetLength(0); i++)
+        int[] map = Enumerable.Repeat(0,max).ToArray();
+        for(int i = 0; i< flatLine.Length; i+=2)
         {
-            for(int j = lines[i,0]; j<=lines[i,1]; j++)
+            for(int j = flatLine[i]+min; j<flatLine[i+1]+min; j++)
             {
-                if(lines[i,0] == j)continue;
-                dLines[j+min]++;
+                map[j]++;
             }
         }
-        Console.WriteLine(string.Join(',',dLines));
-        return dLines.Count(x=> x>1);
-
+        return map.Count((x)=>x> 1);
     }
 }
