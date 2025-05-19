007fec114f46264fd6c7adf14deead6cb1a98c9e
Heoyoon <parechae@gmail.com>
2025-05-19 13:52:57 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.31 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -4,17 +4,15 @@ public class Solution {
     public long solution(int balls, int share) {
         return Comb(balls,share);
     }
-    
     public long Comb(int n, int r)
     {
-        if(r> n-r) r = n-r;
+        if(r > n-r) r = n-r;
         long result = 1;
-        for(int i =1; i <= r;i++)
+        for(int i = 1; i<= r ; i++)
         {
             result *= n-i+1;
             result /= i;
         }
-        
-        return result;
+        return result;        
     }
 }
